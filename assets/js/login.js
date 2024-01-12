b59f928a1c2d61c9822fca8e05438c7f2b12ccc5
TevinH <hanleyt2@gmail.com>
2024-01-12 15:02:41 +0100

    step5

---
+++ assets/js/login.js
@@ -0,0 +1,11 @@
+function validate() {
+    const email = document.forms[0].email.value;
+    const password = document.forms[0].password.value;
+// check 'condition_for_email' and 'condition_for_password'
+    if (email.endsWith('@digitalcareerinstitute.org') && password.length >= 8) {
+        return true;
+     } else {
+        alert("email or password doesn't meet requirements");
+    return false;
+    }
+  }
